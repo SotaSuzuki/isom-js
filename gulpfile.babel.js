1ef5b54014b63683e7d57bf0a24a775f379826d8
SotaSuzuki <3.3sota.suzuki@gmail.com>
2017-09-27 12:54:44 +0900

    bundle the application for clients

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,8 @@ import gulp from 'gulp'
 import babel from 'gulp-babel'
 import nodemon from 'gulp-nodemon'
 import sequence from 'run-sequence'
+import browserify from 'browserify'
+import source from 'vinyl-source-stream'
 
 gulp.task('compile', () => {
   return gulp.src('src/**/*.js')
@@ -17,8 +19,20 @@ gulp.task('copy', () => {
 })
 
 gulp.task('watch', () => {
-  gulp.watch('src/**/*.js', ['compile'])
+  gulp.watch('src/**/*.js', ['compile', 'bundle'])
   gulp.watch('src/**/*.html', ['copy', 'compile'])
+})
+
+gulp.task('bundle', () => {
+  let b = browserify({
+    entries: 'src/index.js',
+    debug: true
+  })
+    .transform('babelify', { presets: ['es2015'] })
+
+  return b.bundle()
+    .pipe(source('build/application.js'))
+    .pipe(gulp.dest('dist'))
 })
 
 gulp.task('start', () => {
@@ -31,5 +45,5 @@ gulp.task('start', () => {
 })
 
 gulp.task('default', (callback) => {
-  sequence(['compile', 'copy', 'watch'], 'start', callback)
+  sequence(['compile', 'copy', 'watch', 'bundle'], 'start', callback)
 })
